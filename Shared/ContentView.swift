e1167fc3319ea9bc8fdb2452afac9bce4553254d
user221968 <bridget.bridget.danver@gmail.com>
2022-05-20 20:25:11 -0400

    Init

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by user221968 on 5/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  Shared
+//
+//  Created by user221968 on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
